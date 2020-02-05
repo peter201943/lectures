1da32931ec378a32d00dd85f30d6c3615d876007
Geoffrey Mainland <mainland@apeiron.net>
2020-02-05 12:45:34 -0500

    Add lecture 9 examples.

---
+++ lecture09/examples.hs
@@ -0,0 +1,97 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Test where
+
+import Prelude hiding (map, filter, take, drop)
+
+--
+-- Functions in Haskell
+--
+fact :: Integer -> Integer
+fact n = if n == 0
+         then 1
+         else n * fact (n-1)
+
+fact' :: Integer -> Integer
+fact' 0 = 1
+fact' n = n * fact' (n-1)
+
+fact'' :: Integer -> Integer
+fact'' n | n == 0    = 1
+         | otherwise = n * fact (n-1)
+
+-- Function with multiple arguments
+
+add3 :: Int -> Int -> Int -> Int
+add3 x y z = x + y + z
+
+ex16 :: Int -> Int -> Int
+ex16 = add3 3
+
+-- Try this:
+--add x = x + "String"
+
+--
+-- List operations
+--
+
+-- map ::
+map = undefined
+
+-- append ::
+append  = undefined
+
+--
+-- Define a safe head function
+--
+
+data MaybeHead = NoHead
+               | JustHead Int
+
+safeHeadInts = undefined
+
+safeHead = undefined
+
+--
+-- More list operations
+--
+
+-- take ::
+take = undefined
+
+-- drop ::
+drop = undefined
+
+--
+-- define a List data type
+--
+
+-- data List
+
+--
+-- define a NonEmptyList data type and associated functions
+--
+
+-- data NonEmptyList
+
+-- nelToList ::
+
+-- listToNel ::
+
+-- headNEL ::
+
+-- tailNEL ::
+
+--
+-- Higher-order functions
+--
+
+-- curry :: ((a, b) -> c) -> a -> b -> c
+
+-- uncurry :: (a -> b -> c) -> (a, b) -> c
+
+-- (.) :: (b -> c) -> (a -> b) -> a -> c
+
+-- ($) :: (a -> b) -> a -> b
+
+-- What about papply?
